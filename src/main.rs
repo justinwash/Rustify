82909a211d158ba94f77d1d5d86e5907cbfd9e09
Justin Wash <justin.wash@pm.me>
2019-07-26 16:35:29 -0500

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate webview;
+
+use webview::{Content, WebView};
+
 fn main() {
-    println!("Hello, world!");
+    let html = format!(
+        include_str!("./web/index.html"),
+        style = include_str!("./web/index.css"),
+        script = include_str!("./web/index.js") 
+    );
+
+    let view = WebView::new(
+        "Rustify",                               // The title of the window
+        Content::Html(html), // The content to display
+        480,                                     // Width
+        640,                                     // Height
+        true,                                    // Resizable?
+        true,                                    // Debugable?
+    )
+    .unwrap();
+
+    // Starts the event loop
+    view.join();
 }
